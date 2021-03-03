1161b42e838fb3aa9460163ee16a77ed34ff9cd6
dmiska25 <djmiska25@gmail.com>
2021-03-03 15:50:20 -0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { MatCardModule } from "@angular/material/core";
+import { MatCardModule } from '@angular/material';
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
